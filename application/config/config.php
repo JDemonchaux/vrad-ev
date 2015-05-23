9a329d250f96f6a5c50f132c4e64054be7844258
Jerome Demonchaux <jerome.demonchaux@gmail.com>
2015-05-23 18:46:44 +0200

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -16,7 +16,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/CustomCI-MBJD/';
+$config['base_url'] = 'http://localhost/vrad-ev/';
 
 /*
 |--------------------------------------------------------------------------
