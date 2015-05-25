a512809389288fd32418d1ebe4edd048dde2afdc
MB-work <mariebarbier@MacBook-Pro-de-Marie.local>
2015-05-25 11:16:52 +0200

    corrections

--- application/controllers/Notation_v1-1/Ressources.php
+++ application/controllers/Notation_v1-1/Ressources.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-  class Ressouces extends CI_Controller {
+  class Ressources extends CI_Controller {
 
       public $module = "Notation";
 
