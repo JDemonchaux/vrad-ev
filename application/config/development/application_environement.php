ab03688ddb00c07391840c2d3dce05ab4514f793
MB-work <mariebarbier@MacBook-Pro-de-Marie.local>
2015-05-25 11:13:01 +0200

    test

--- application/config/development/application_environement.php
+++ application/config/development/application_environement.php
@@ -1,6 +1,6 @@
 <?php
 //ecraser le site URL (config appartenant à l'environement)
-$config['base_url'] = 'https://vrad-ev.com/';
+$config['base_url'] = 'http://localhost/vrad-ev/';
 
 
 $config['nothing_for_the_moment'] = '';
