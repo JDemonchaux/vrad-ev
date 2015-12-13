96debbf71eee7d1b9f518934b1c6122a335ac421
Shorty-fr <jerome.demonchaux@gmail.com>
2015-12-13 13:20:55 +0100

    Vue podium 100%

--- application/views/Notation_v1-1/podium.php
+++ application/views/Notation_v1-1/podium.php
@@ -1,5 +1,5 @@
 <?php
-//var_dump($les_groupes);
+
 ?>
 
 <div class="row">
@@ -9,21 +9,22 @@
                 &nbsp;
             </div>
             <div class="col-sm-10">
-                <div class="row podium">
+                <div class="row row-podium  ">
                     <?php $i = 0;
                     foreach ($les_groupes as $groupe) {
                     $i++;
                     if ($i < 4) { ?>
                     <?php if ($i == 1) {
-                        echo '<div class="col-sm-4 col-sm-push-4">';
+                        echo '<div class="col-sm-4 col-sm-push-4 podium">';
                     } else if ($i == 2) {
-                        echo '<div class="col-sm-4 col-sm-pull-4" >';
+                        echo '<div class="col-sm-4 col-sm-pull-4 podium">';
                     } else {
-                        echo '<div class="col-sm-4">';
+                        echo '<div class="col-sm-4 podium">';
                     } ?>
                     <div class="no<?php echo $i; ?>">
-                        <div
-                            class="logo"><?php echo img_url("ecoles/" . $groupe->getEcole()->getId() . ".png", "Logo de l'école", "logoEcole"); ?></div>
+                        <div class="logo">
+                            <?php echo img_url("ecoles/" . $groupe->getEcole()->getId() . ".png", "Logo de l'école", "logoEcole"); ?>
+                        </div>
                         <div class="title"><?php echo $groupe->getLibelle(); ?></div>
                         <div class="score"><?php echo $groupe->getScore(); ?>/200</div>
                         <div class="progress ">
@@ -42,19 +43,35 @@
                         <div class="place">
                             <?php
                             if ($i == 1) {
-                                echo "1er";
-                            }
-                            elseif ($i == 2) {
-                                echo "2ème";
-                            }
-                            elseif ($i == 3) {
-                                echo "3ème";
+                                echo "<div class='medal n1'>".$i."</div>";
+                            } elseif ($i == 2) {
+                                echo "<div class='medal n2'>".$i."</div>";
+                            } elseif ($i == 3) {
+                                echo "<div class='medal n3'>".$i."</div>";
                             }
 
                             ?>
                         </div>
                     </div>
                 </div>
+                <?php } else { ?>
+                    <div class="col-sm-2 classement">
+                        <div class="logo">
+                            <?php echo img_url("ecoles/" . $groupe->getEcole()->getId() . ".png", "Logo de l'école", "logoEcole"); ?>
+                        </div>
+                        <div class="title"><?php echo $groupe->getLibelle(); ?></div>
+                        <div class="score"><?php echo $groupe->getScore(); ?>/200</div>
+                        <div class="progress ">
+                            <div class="progress-bar" role="progressbar"
+                                 aria-valuenow="<?php echo $groupe->getAvancement(); ?>"
+                                 aria-valuemin="0" aria-valuemax="100"
+                                 style="width: <?php echo $groupe->getAvancement(); ?>%;">
+                                            <span class="avancement">
+                                                <?php echo $groupe->getAvancement(); ?>%
+                                            </span>
+                            </div>
+                        </div>
+                    </div>
                 <?php }
                 } ?>
 
